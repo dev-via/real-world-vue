28daf39836a5c89bb646fd7b635a1574c08747c7
xray821 <85374989+xray821@users.noreply.github.com>
2021-07-17 19:28:50 +0200

    Update EventService.js

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/Code-Pop/Real-World_Vue-3',
+  //baseURL: 'https://my-json-server.typicode.com/Code-Pop/Real-World_Vue-3',
+  baseURL: 'https://my-json-server.typicode.com/xray821/real-world-vue',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
